3a5c2936aa79fc0396ff7535d54d59444eb110cb
Lucas <104849791+lucasmorenoo@users.noreply.github.com>
2023-03-08 18:54:36 -0300

    initial commit

---
+++ js/function.js
@@ -0,0 +1,14 @@
+document.addEventListener('click', function() {
+    console.log("Hola mundo desde EventListener");
+    document.getElementById("demo").innerHTML ="¿COMO INICIAR MIS INVESTIGACIONES EN LA BOLSA?"
+})
+
+
+
+/** document.getElementById('boton_color').addEventListener('click', function(){
+document.body.style.backgroundColor = '#FF0000';
+});
+
+document.getElementById('boton_default').addEventListener('click', function(){
+document.body.style.backgroundColor = 'green';
+}); */
